01ac15880e13a51aa17c546c76644ebda15fdb19
arisromil <arisromil@yahoo.com>
2023-12-21 09:02:22 -0700

    Create ChallengeService.java

---
+++ src/main/java/multiplication/challenge/ChallengeService.java
@@ -0,0 +1,13 @@
+package multiplication.challenge;
+
+public interface ChallengeService {
+/**
+ * Verifies if an attempt coming from the presentation layer is
+ correct or not.
+ *
+ * @return the resulting ChallengeAttempt object
+ * */
+
+
+    ChallengeAttempt verifyAttempt(ChallengeAttemptDTO resultAttempt);
+}
