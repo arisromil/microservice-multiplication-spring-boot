73c3b8adfd465b9c14044743afa9bf17c3c219c3
arisromil <arisromil@yahoo.com>
2023-12-22 10:28:44 -0700

    Create ChallengeServiceImpl.java

---
+++ src/main/java/multiplication/challenge/ChallengeServiceImpl.java
@@ -0,0 +1,23 @@
+package multiplication.challenge;
+
+import multiplication.user.User;
+
+public class ChallengeServiceImpl implements ChallengeService {
+    @Override
+    public ChallengeAttempt verifyAttempt(ChallengeAttemptDTO attemptDTO) {
+        // Check if the attempt is correct
+        boolean isCorrect = attemptDTO.getGuess() ==
+                attemptDTO.getFactorA() * attemptDTO.getFactorB();
+        // We don't use identifiers for now
+        User user = new User(null, attemptDTO.getUserAlias());
+        // Builds the domain object. Null id for now.
+        ChallengeAttempt checkedAttempt = new ChallengeAttempt(null,
+                user,
+                attemptDTO.getFactorA(),
+                attemptDTO.getFactorB(),
+                attemptDTO.getGuess(),
+                isCorrect
+        );
+        return checkedAttempt;
+    }
+}
