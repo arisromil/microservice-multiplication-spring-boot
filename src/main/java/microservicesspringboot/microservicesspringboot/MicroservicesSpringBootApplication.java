6f76adeb8d2d3843e4839d7bf3e25a0253f93ad3
arisromil <arisromil@yahoo.com>
2023-12-22 10:27:57 -0700

    Injecting Beans in the Context to Override Defaults in Spring Boot

--- src/main/java/microservicesspringboot/microservicesspringboot/MicroservicesSpringBootApplication.java
+++ src/main/java/microservicesspringboot/microservicesspringboot/MicroservicesSpringBootApplication.java
@@ -1,13 +1,23 @@
 package microservicesspringboot.microservicesspringboot;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MicroservicesSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroservicesSpringBootApplication.class, args);
+	}
+
+	@Bean
+	public ObjectMapper objectMapper() {
+		var om = new ObjectMapper();
+		om.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+		return om;
 	}
 
 }
