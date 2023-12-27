5af09ba151f8406c57fe4a09e3690bee38814157
arisromil <arisromil@yahoo.com>
2023-12-27 00:30:36 -0700

    make the User and ChallengeAttempt classes become entities

--- src/main/java/multiplication/challenge/ChallengeAttempt.java
+++ src/main/java/multiplication/challenge/ChallengeAttempt.java
@@ -1,20 +1,25 @@
 package multiplication.challenge;
 
+import jakarta.persistence.*;
 import lombok.*;
 import multiplication.user.User;
 
 /**
  * Identifies the attempt from a {@link User} to solve a challenge.
  */
-@Getter
-@ToString
+@Entity
+@Data
 @EqualsAndHashCode
 @AllArgsConstructor
 public class ChallengeAttempt {
+    @Id
+    @GeneratedValue
     private Long id;
-    private User userId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID")
+    private User user;
     private int factorA;
     private int factorB;
-    private int resultAttempt;
+    private int guess;
     private boolean correct;
 }
--- src/main/java/multiplication/user/User.java
+++ src/main/java/multiplication/user/User.java
@@ -1,14 +1,23 @@
 package multiplication.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.*;
 /**
  * Stores information to identify the user.
  */
-@Getter
-@ToString
+
+@Entity
+@Data
 @EqualsAndHashCode
 @AllArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue
     private Long id;
     private String alias;
+    public User(final String userAlias) {
+        this(null, userAlias);
+    }
 }
