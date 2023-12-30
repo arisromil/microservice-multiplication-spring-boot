b61bd5e969250832e64b54d3d9cccebd8623eec6
arisromil <arisromil@yahoo.com>
2023-12-30 00:16:45 -0700

    Using Mockito

--- src/test/java/multiplication/challenge/ChallengeServiceTest.java
+++ src/test/java/multiplication/challenge/ChallengeServiceTest.java
@@ -2,13 +2,33 @@ package multiplication.challenge;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import static org.assertj.core.api.BDDAssertions.then;
 
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
 public class ChallengeServiceTest {
     private ChallengeService challengeService;
+
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private ChallengeAttemptRepository attemptRepository;
+
     @BeforeEach
     public void setUp() {
-        challengeService = new ChallengeServiceImpl();
+        challengeService = new ChallengeServiceImpl(
+                userRepository,
+                attemptRepository
+        );
+        given(attemptRepository.save(any()))
+                .will(returnsFirstArg());
     }
     @Test
     public void checkCorrectAttemptTest() {
